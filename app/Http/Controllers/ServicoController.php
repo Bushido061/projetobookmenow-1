8b61f69fb5c1bdae21d905ec73d5f659b4db7242
Bushido061 <huds291@gmail.com>
2024-08-19 22:03:33 -0300

    login

---
+++ app/Http/Controllers/ServicoController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Servico;
+use Illuminate\Http\Request;
+
+class ServicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $servicos = Servico::paginate(10);
+        return view('admin.servicos.index', compact('servicos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.servicos.cadastrar');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required|string|max:100',
+            'descricao' => 'required',
+            'valor' => 'required|string|max:20',
+            'celular' => 'required|string|max:20',
+            'endereco' => 'required',
+            'numero' => 'required',
+            'complemento' => 'required',
+            'bairro' => 'required',
+            'cidade' => 'required',
+            'estado' => 'required',
+            'cep' => 'required',
+            'usuario_id' => 'required',
+            'categoria_id' => 'required',
+
+        ]); 
+
+        Servico::create($request->all());
+
+        return redirect()->route('servico.index')->with('sucesso', 'Usuário cadastrado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $servico = Servico::findOrFail($id);
+        return view('admin.servicos.visualizar', compact('servico'));
+
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
