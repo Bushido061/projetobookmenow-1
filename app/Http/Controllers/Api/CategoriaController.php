b634361a8bb1181da3b2406a7f1f2da1798eb956
Bushido061 <huds291@gmail.com>
2024-10-23 22:11:53 -0300

    rotas de api criadas no categoria

--- app/Http/Controllers/Api/CategoriaController.php
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -12,7 +12,7 @@ class CategoriaController extends Controller
     {
         $categorias = Categoria::all();
         try {
-            return reponse()->json([$categorias, 200]);
+            return response()->json([$categorias, 200]);
         } catch(Exception $e) {
             return request()->json(['Erro' => "Erro ao listar os dados", 500]);
         }  
@@ -27,5 +27,81 @@ class CategoriaController extends Controller
             return request()->json(['Erro' => "Erro ao listar os dados", 400]);
         }
     }
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'titulo' => 'required',
+            'descricao' => 'required',
+            'imagem' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        try {
+            
+       
+        $data = [
+            'titulo' => $request->titulo,
+            'descricao' => $request->descricao,
+        ];
+   
+        if ($request->hasFile('imagem')) {
+            $caminhoImagem = $request->file('imagem')->store('categorias', 'public');
+            $data['imagem'] = "/storage/".$caminhoImagem; // Salva o caminho da imagem
+        }
+
+ 
+       $categoria = Categoria::create($data);
+       
+               return  response()->json($categoria,201);
+
+    } catch (Exception $e) {
+        return request()->json(['Erro' => "Erro ao listar os dados", 500]);
+   }
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'descricao' => 'required',
+            'imagem' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        try {
+        $categoria = Categoria::findOrFail($id);
+
+        $data = [
+            'titulo' => $request->titulo,
+            'descricao' => $request->descricao,
+        ];
+
+        if ($request->hasFile('imagem')) {
+            // Apaga a imagem antiga, se existir
+            if ($categoria->imagem) {
+                Storage::disk('public')->delete($categoria->imagem);
+            }
+
+            $caminhoImagem = $request->file('imagem')->store('categorias', 'public');
+            $data['imagem'] = "/storage/".$caminhoImagem; // Salva o novo caminho da imagem
+        }
+
+        $categoria->update($data);
+
+        return response()->json($categoria, 200);
+        
+    } catch (Exception $e) {
+        return request()->json(['Erro' => "Erro ao listar os dados", 500]);
+   }
+    }
+    public function destroy(string $id)
+    {
+        try {
+            $categoria = Categoria::findOrFail($id);
+            $categoria->delete();
+            return response()->json(["message" => "Categoria deletada com sucesso", 200]);
+        } catch (Exception $e) {
+ 
+            return response()->json(["error" => "Erro ao deletar categoria", 200]);
+        }
+    }
+
 }
  
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\Api\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("categorias", [CategoriaController::class, "index"]);
+Route::post("categorias", [CategoriaController::class,"store"]);
 Route::get("categorias/{id}", [CategoriaController::class, "show"]);
+Route::put("categorias/{id}", [CategoriaController::class, "update"]);
+Route::delete("categorias/{id}", [CategoriaController::class, "destroy"]);
